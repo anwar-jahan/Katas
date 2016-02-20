4d175a5459f71a20a85c13df203e1c0c7b7ae45f
du2jahan <du2jahan@gmail.com>
2016-02-20 15:08:50 -0500

    Tennis with Single Test Case

--- KataTennis/src/main/tennis/KataTennisGame.java
+++ KataTennis/src/main/tennis/KataTennisGame.java
@@ -1,13 +1,52 @@
 package main.tennis;
 
 public class KataTennisGame {
-	
-	
-	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Player playerOne;
+	Player playerTwo;
 
+	public KataTennisGame(Player playerOne, Player playerTwo) {
+		this.playerOne = playerOne;
+		this.playerTwo = playerTwo;
+	}
+
+	public String getScoreResults() {
+
+		if (playerOne.getScore() >= 3 && playerTwo.getScore() >= 3) {
+			if((playerTwo.getScore()-playerOne.getScore())>=2){
+				return advancePlayer().getName()+" : Win and Score:"+scoreTransfer(advancePlayer().getScore());
+			}
+			else if(playerTwo.getScore()==playerOne.getScore()){
+				return "Deuce";
+			}
+			else{
+				return "Advantage :"+advancePlayer().getName()+" And Score: "+scoreTransfer(advancePlayer().getScore());
+			}
+		} else {
+			return "PlayerName :"+playerOne.getName()+" Score:"+scoreTransfer(playerOne.getScore())+" PlayerName :"+playerTwo.getName()+" Score:"+scoreTransfer(playerTwo.getScore()) ;
+		}
+	}
+
+	public Player advancePlayer() {
+		return (playerOne.getScore() > playerTwo.getScore()) ? playerOne
+				: playerTwo;
+	}
+	
+	public String scoreTransfer(int score) throws IllegalArgumentException{
+		switch(score){
+		case 0: return "Love";
+		case 1: return "Fifteen";
+		case 2: return "Thirty";
+		case 3: return "Forty";
+		}
+		return "Illegal Score "+score;
+		
+	}
+	public static void main(String[] args){
+		Player playerOne=new Player("Anwar", 0);
+		Player playerTwo=new Player("Jahan", 0);
+		KataTennisGame game=new KataTennisGame(playerOne, playerTwo);
+		System.out.println(game.getScoreResults());
 	}
 
 }
--- KataTennis/src/test/tennis/KataTennisGameTest.java
+++ KataTennis/src/test/tennis/KataTennisGameTest.java
@@ -1,5 +1,27 @@
 package test.tennis;
 
-public class KataTennisGameTest {
+import org.junit.Before;
+import org.junit.Test;
+
+import main.tennis.KataTennisGame;
+import main.tennis.Player;
+import junit.framework.TestCase;
+
+public class KataTennisGameTest extends TestCase {
+	Player playerOne;
+	Player playerTwo;
+	KataTennisGame game;
+
+	@Before
+	public void setup() {
+		playerOne=new Player("Anwar", 0);
+		playerTwo=new Player("Jahan", 0);
+		game=new KataTennisGame(playerOne, playerOne);
+	}
+	
+	@Test
+	public void loveScoreTest(){
+		assertEquals("PlayerName :Anwar Score:Love PlayerName :Jahan Score:Love",game.getScoreResults());
+	}
 
 }
